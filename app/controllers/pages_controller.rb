948c9581745badfdab00716428d2f2ba61959346
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2022-05-12 14:54:31 -0400

    model created and connected with the view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   end
 
   def create
+    Project.create(name: params[:name], description: params[:description], start: params[:start], end: params[:end], state: params[:state])
   end
 
   def dashboard
