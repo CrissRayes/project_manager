e2d20ad578e5adc4ed2cb83a8267af621a824f1e
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2022-05-12 15:26:19 -0400

    change_column applied to description

---
+++ db/migrate/20220512192202_change_type_description.rb
@@ -0,0 +1,5 @@
+class ChangeTypeDescription < ActiveRecord::Migration[7.0]
+  def change
+    change_column :projects, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_12_183740) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_12_192202) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
-    t.string "description"
+    t.text "description"
     t.date "start"
     t.date "end"
     t.integer "state"
